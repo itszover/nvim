a61595052b5a1b2f1221f211a6f643fb2ac32634
itszover <jose.martinho@outlook.com.br>
2025-08-24 14:56:32 -0300

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+	
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = { "lua", "go" },
+		auto_install = false,
+		highligth = { enabled = true },
+		indent = { enabled = true }
+	}
+}
