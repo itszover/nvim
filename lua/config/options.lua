c99ec12620f4d7611365df3e6e968ceaef004c3a
itszover <jose.martinho@outlook.com.br>
2025-08-26 13:06:02 -0300

    removed lsp.lua and .gitignore

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,13 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+
+vim.g.have_nerd_font = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,11 @@
 return {
-	
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = { "lua", "go" },
-		auto_install = false,
-		highligth = { enabled = true },
-		indent = { enabled = true }
-	}
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = { "lua", "go" },
+        auto_install = false,
+        highligth = { enabled = true },
+        indent = { enabled = true }
+    }
 }
