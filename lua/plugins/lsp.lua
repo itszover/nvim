1df303724c7ad6e19a87ba30145cfb5df8606dbd
itszover <jose.martinho@outlook.com.br>
2025-08-26 17:50:28 -0300

    add minimal lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"folke/lazydev.nvim",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
+		end
+	}
+}
