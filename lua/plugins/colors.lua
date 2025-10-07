de040a7417805c7898db5daa50908267b2953a96
itszover <jose.martinho@outlook.com.br>
2025-10-07 16:25:07 -0300

    change colorscheme to gruvbox

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,10 +1,7 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
 	config = function()
-		require("rose-pine").setup({
-		styles = { transparency = true }
-	})
-		vim.cmd("colorscheme rose-pine")
+		vim.cmd.colorscheme("gruvbox")
 	end
 }
